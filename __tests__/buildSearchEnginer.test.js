47a8622c1ca06ab42dc7b6378c8d0466ba0114a8
Serge <liho@ro.ru>
2021-06-22 00:37:41 +0300

    feature word processing

--- __tests__/buildSearchEnginer.test.js
+++ __tests__/buildSearchEnginer.test.js
@@ -1,9 +1,15 @@
 import buildSearchEngine from '../src/buildSearchEngine';
 import docs from '../__fixtures__/docs';
 
+const searchEngine = buildSearchEngine(docs);
+const searchEngine2 = buildSearchEngine([]);
+
 test('create search engine object', () => {
-  const searchEngine = buildSearchEngine(docs);
-  const searchEngine2 = buildSearchEngine([]);
   expect(searchEngine.search('shoot')).toEqual(['doc1', 'doc2']);
   expect(searchEngine2.search('')).toEqual([]);
+});
+
+test('words processing', () => {
+  expect(searchEngine.search('pint')).toEqual(['doc1']);
+  expect(searchEngine.search('pint!')).toEqual(['doc1']);
 });
--- src/buildSearchEngine.js
+++ src/buildSearchEngine.js
@@ -1,10 +1,16 @@
 export default (docs) => (
   {
     coll: docs,
+    processedText(text) {
+      return text.match(/\w+/g);
+    },
     search(str) {
-      if (str === '') return [];
-      return this.coll
-        .filter(({ text }) => text.split(' ').includes(str))
+      if (str === '') {
+        return [];
+      }
+
+      const [processedStr] = this.processedText(str);
+      return this.coll.filter(({ text }) => this.processedText(text).includes(processedStr))
         .map(({ id }) => id);
     },
   }
