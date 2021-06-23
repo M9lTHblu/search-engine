5eb4eff198a212efcc30f955bcb77233199643df
Serge <liho@ro.ru>
2021-06-23 21:02:18 +0300

    add relevance metric

--- __tests__/buildSearchEnginer.test.js
+++ __tests__/buildSearchEnginer.test.js
@@ -4,12 +4,9 @@ import docs from '../__fixtures__/docs';
 const searchEngine = buildSearchEngine(docs);
 const searchEngine2 = buildSearchEngine([]);
 
-test('create search engine object', () => {
-  expect(searchEngine.search('shoot')).toEqual(['doc1', 'doc2']);
+test('relevance metric', () => {
   expect(searchEngine2.search('')).toEqual([]);
-});
-
-test('words processing', () => {
   expect(searchEngine.search('pint')).toEqual(['doc1']);
   expect(searchEngine.search('pint!')).toEqual(['doc1']);
+  expect(searchEngine.search('shoot')).toEqual(['doc2', 'doc1']);
 });
--- src/buildSearchEngine.js
+++ src/buildSearchEngine.js
@@ -1,17 +1,20 @@
+import _ from 'lodash';
+
 export default (docs) => (
   {
     coll: docs,
-    processedText(text) {
-      return text.match(/\w+/g);
-    },
+    processText: (text) => text.match(/\w+/g),
     search(str) {
       if (str === '') {
         return [];
       }
-
-      const [processedStr] = this.processedText(str);
-      return this.coll.filter(({ text }) => this.processedText(text).includes(processedStr))
-        .map(({ id }) => id);
+      const [processedStr] = this.processText(str);
+      const filtered = this.coll.filter(({ text }) => this.processText(text).includes(processedStr))
+        .map((item) => {
+          const count = _.words(item.text).filter((word) => word === processedStr).length;
+          return { ...item, metricCount: count };
+        });
+      return _.sortBy(filtered, 'metricCount').map(({ id }) => id).reverse();
     },
   }
 );
