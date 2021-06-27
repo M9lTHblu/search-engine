acd354529a62df83859b30c0a95b1be99b5efdfa
Serge <liho@ro.ru>
2021-06-27 20:32:51 +0300

    add invert index

--- src/buildSearchEngine.js
+++ src/buildSearchEngine.js
@@ -1,24 +1,58 @@
-export default (docs) => (
+import union from 'lodash/union';
+
+export default (data) => (
   {
-    coll: docs,
+    docs: data,
+
+    process(str) {
+      return str.match(/\w+/g);
+    },
+
+    ranging(words, docs) {
+      return (
+        docs.map((doc) => {
+          const matches = words.reduce((acc, word) => {
+            const count = this.process(doc.text).filter((wordOfDoc) => wordOfDoc === word).length;
+            return acc + count;
+          }, 0);
+          return { ...doc, matches };
+        })
+      );
+    },
+
+    sort(docs) {
+      return docs
+        .filter((doc) => doc.matches > 0)
+        .sort((docA, docB) => docB.matches - docA.matches);
+    },
+
+    invertIndex(docs) {
+      const texts = docs.flatMap(({ text }) => this.process(text));
+
+      return texts.reduce((inverted, word) => {
+        const filteredDocs = docs.filter(({ text }) => text.includes(word));
+        return { ...inverted, [word]: filteredDocs };
+      }, {});
+    },
 
     search(str) {
       if (str === '') {
         return [];
       }
+      const processedStr = this.process(str);
+      const invertedIndex = this.invertIndex(this.docs);
+      const keywords = Object.keys(invertedIndex);
 
-      return this.coll
-        .map((item) => {
-          const count = str
-            .match(/\w+/g)
-            .reduce((acc, word) => (item.text.match(/\w+/g).includes(word) ? acc + 1 : acc), 0);
+      const actualDocs = processedStr.reduce((acc, word) => {
+        if (keywords.includes(word)) {
+          return union(acc, invertedIndex[word]);
+        }
+        return acc;
+      }, []);
 
-          return { ...item, count };
-        })
-        .filter((item) => item.count > 0)
-        .sort((a, b) => a.count - b.count)
-        .map(({ id }) => id)
-        .reverse();
+      const rangingDocs = this.ranging(processedStr, actualDocs);
+
+      return this.sort(rangingDocs).map(({ id }) => id);
     },
   }
 );
