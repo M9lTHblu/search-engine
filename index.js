bb869a0f10b6e6bca9be99aec09969f0f77fc2ae
Serge <liho@ro.ru>
2021-07-12 21:06:43 +0300

    make prototype

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
+
 import buildSearchEngine from './src/buildSearchEngine';
 
 export default buildSearchEngine;
-=======
-import buildSearchEngine from './src/buildSearchEngine';
 
-export default buildSearchEngine;
->>>>>>> cf5e00a00167589405b0361da3d59f34c213f118
