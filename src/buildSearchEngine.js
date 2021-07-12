79d310c284eb8024880f8c0d454f4ddf99dc206f
Serge <liho@ro.ru>
2021-07-12 21:18:50 +0300

    fix lint

--- src/buildSearchEngine.js
+++ src/buildSearchEngine.js
@@ -1,38 +1,27 @@
 import _ from 'lodash';
 
-export default (documents) => new Search(documents);
-
-class Search {
-  constructor(data = []) {
-    this.docs = data
-    this.termsAndDocs = this.invertIndex(data)
-  }
-
+export default (data) => ({
+  docs: data,
+  termsAndDocs: this.invertIndex(data),
   getDocsCount(docs) {
     return docs.length;
-  }
-
+  },
   getTermDocsCount(term) {
     return this.termsAndDocs[term].length;
-  }
-
+  },
   getWordsOfDocCount(docId) {
     const { text } = this.docs[docId];
     return this.process(text).length;
-  }
-
+  },
   getMatchedDocs(term) {
     return this.docs
       .filter(({ text }) => this.process(text).includes(term));
-  }
-
+  },
   process(str) {
-    return _.words(str.toLowerCase())
-  }
-
+    return _.words(str.toLowerCase());
+  },
   invertIndex() {
-    const allWordsOfDocs = this.docs.flatMap(({ text }) =>
-      _.uniq(this.process(text)));
+    const allWordsOfDocs = this.docs.flatMap(({ text }) => _.uniq(this.process(text)));
 
     const termsAndDocs = allWordsOfDocs.reduce((acc, term) => {
       const matchedDocs = this.getMatchedDocs(term);
@@ -42,31 +31,34 @@ class Search {
 
       const addedMetrics = matchedDocs.map((doc) => {
         const words = this.process(doc.text);
-        const tf = words.filter(word => word === term).length / words.length;
-        return { ...doc, tf, idf, tfIdf: tf * idf }
-      })
+        const tf = words.filter((word) => word === term).length / words.length;
+        return {
+          ...doc, tf, idf, tfIdf: tf * idf,
+        };
+      });
 
-      return { ...acc, [term]: addedMetrics }
+      return { ...acc, [term]: addedMetrics };
     }, {});
 
-    return termsAndDocs
-  }
-
+    return termsAndDocs;
+  },
   search(str) {
     if (str === '') {
       return [];
     }
     const res = this.process(str).flatMap((term) => {
       const docs = this.termsAndDocs[term];
-      return  docs.reduce((acc, { id, tfIdf }) => {
+      return docs.reduce((acc, { id, tfIdf }) => {
         if (acc[id]) {
           acc[id] += tfIdf;
         } else {
           acc[id] = tfIdf;
         }
-        return acc
-      }, {})
-    })
-    return Object.entries(res[0]).sort(([, value1], [, value2]) => value2 - value1).map(([id, ]) => id)
-  }
-}
+        return acc;
+      }, {});
+    });
+    return Object.entries(res[0])
+      .sort(([, value1], [, value2]) => value2 - value1)
+      .map(([id]) => id);
+  },
+});
